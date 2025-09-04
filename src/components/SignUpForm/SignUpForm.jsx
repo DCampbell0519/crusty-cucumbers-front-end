3b4a67cc822ab6f00601ea982b8e7156425421bd
Donovan Campbell <donovan@dlcentertainment.net>
2025-09-03 20:57:59 -0700

    env file, signup form started, react router installed

---
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router'
+
+const SignUpForm = () => {
+  return (
+    <div>SignUpForm</div>
+  )
+}
+
+export default SignUpForm
