d61df50e3dcfd94d062a4ce07ccc43f5b9fce27b
Donovan Campbell <donovan@dlcentertainment.net>
2025-09-04 19:05:32 -0700

    sign up form complete

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,11 +1,131 @@
-import React from 'react'
-import { useState } from 'react'
-import { useNavigate } from 'react-router'
+import React from "react";
+import { useContext } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const SignUpForm = () => {
-  return (
-    <div>SignUpForm</div>
-  )
-}
+  const navigate = useNavigate();
+  const [message, setMessage] = useState("");
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    password: "",
+    passwordConf: "",
+  });
 
-export default SignUpForm
+  // const { setUser } = useContext(UserContext);
+
+  const { firstName, lastName, email, username, password, passwordConf } =
+    formData;
+
+  const handleChange = (event) => {
+    setMessage("");
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const newUser = await signUp(formData);
+      setUser(newUser);
+      console.log(newUser);
+      navigate("/");
+    } catch (error) {
+      setMessage(error.message);
+    }
+  };
+
+  const isFormInvalid = () => {
+    return !(
+      firstName &&
+      lastName &&
+      email &&
+      username &&
+      password &&
+      password === passwordConf
+    );
+  };
+
+  return (
+    <main>
+      <h1>Become a Cucumber</h1>
+      <p>{message}</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={firstName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email Address:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="confirm">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirm"
+            name="passwordConf"
+            value={passwordConf}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <button disabled={isFormInvalid()}>Sign Up</button>
+          <button onClick={() => navigate("/")}>Cancel</button>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default SignUpForm;
