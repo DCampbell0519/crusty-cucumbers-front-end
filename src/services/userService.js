4a6afde773537b7746f933d33c3763954512a444
Donovan Campbell <donovan@dlcentertainment.net>
2025-09-06 16:31:31 -0700

    started dashboard

---
+++ src/services/userService.js
@@ -0,0 +1,25 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_URL}/user`;
+
+const index = async () => {
+    try {
+        const res = await fetch(BASE_URL, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            } 
+        })
+
+        const data = res.json();
+
+        if (data.error) {
+            throw new Error(data.error)
+        }
+
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export {
+    index,
+}
