44fe52021ae7504faab455719a006e762d0f960e
Donovan Campbell <donovan@dlcentertainment.net>
2025-09-07 23:32:23 -0700

    Edit profile button and functionality, tested and verified

--- src/services/userService.js
+++ src/services/userService.js
@@ -41,7 +41,32 @@ const getMyProfile = async () => {
     }
 }
 
+const updateMyProfile = async (profileData) => {
+    try {
+        const res = await fetch(`${BASE_URL}/profile`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+            body: JSON.stringify(profileData)
+        });
+
+        const data = await res.json();
+
+        if (data.error) {
+            throw new Error(data.error)
+        }
+
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export {
     index,
     getMyProfile,
+    updateMyProfile,
 }
